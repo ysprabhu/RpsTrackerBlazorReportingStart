abd7acdfd50d94bcdf17a041a4de0c8bb6fbb5a1
Yogesh Prabhu <yogesh.prabhu@genexservices.com>
2024-06-28 17:00:04 -0700

    Add a sample Telerik report that connects to database and display it from Blazor component.

---
+++ RPS.Web.Server/Controller/ReportsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Telerik.Reporting.Services;
+using Telerik.Reporting.Services.AspNetCore;
+
+namespace RPS.Web.Server.Controller
+{
+  [Route("api/reports")]
+  [ApiController]
+  public class ReportsController : ReportsControllerBase
+  {
+        public ReportsController(IReportServiceConfiguration reportServiceConfig) : base(reportServiceConfig)
+        {
+            
+        }
+    }
+}
